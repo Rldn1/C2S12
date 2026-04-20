ab4872a84fedd107614ca1a7c14d6d04d9bfb727
Esmeralda <roldanesmeralda71@gmail.com>
2026-04-19 18:33:31 -0600

    actualizacion

---
+++ c2semana12/resources/views/newpag.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pág. 2</title>
+</head>
+<body>
+    <h1>Volver a la página principal</h1>
+  <form action="/" method="get">
+    <button type="submit">Click</button>
+</form>
+</body>
+</html>
---
+++ c2semana12/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Pág. con Laravel</title>
+</head>
+<body>
+    <h1>Bienvenido/a a mi primer página usando Laravel</h1>
+    <p>Este es un parráfo de prueba solamente.</p>
+    <a href="{{route ('mipagina')}}">Ir a la otra página</a>
+</body>
+</html>
+
